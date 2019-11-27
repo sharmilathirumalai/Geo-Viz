b89e18d4a78b018a8f835e9579f83c0c782f6242
Sharmila Thirumalainathan <sh946344@dal.ca>
2019-11-26 20:41:48 -0400

    Density stats Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VizComponent } from './viz/viz.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @NgModule({
   declarations: [
--- src/app/viz/viz.component.ts
+++ src/app/viz/viz.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import 'node_modules/leaflet-choropleth/dist/choropleth.js'
 import { StoreService } from '../store.service';
+import * as $ from 'jquery';
+import Chart from 'chart.js';
 
 declare let L;
 
@@ -15,6 +17,8 @@ export class VizComponent implements OnInit {
   map: any;
   year: number;
   week: number;
+  median: number;
+  max = 0;
 
   constructor(private store: StoreService) {
 
@@ -22,10 +26,12 @@ export class VizComponent implements OnInit {
 
   convertTOGeoJSON(polygons) {
     let features = [];
+    let totalValues = [];
 
     for (var i = 0; i < polygons.result.length; i++) {
       let polygon = polygons.result[i];
       if (polygon.value > 0) {
+
         let properties = { density: polygon.value };
         let geometry = {
           "type": "Polygon",
@@ -35,10 +41,15 @@ export class VizComponent implements OnInit {
           "type": "Feature",
           properties,
           geometry
-        })
+        });
+
+        this.max = this.max < polygon.value ? polygon.value : this.max;
+
+        totalValues.push(polygon.value);
       }
     }
 
+    this.computeMedian(totalValues);
     return {
       "type": "FeatureCollection",
       "features": features
@@ -46,17 +57,34 @@ export class VizComponent implements OnInit {
 
   }
 
+  computeMedian(values) {
+    values.sort((a, b) => a - b);
+    let lowMiddle = Math.floor((values.length - 1) / 2);
+    let highMiddle = Math.ceil((values.length - 1) / 2);
+    this.median = (values[lowMiddle] + values[highMiddle]) / 2;
+  }
+
   ngOnInit() {
-    this.map = L.map('mapid').setView([52.00118, -87.359296], 4);
+
+    var maxBounds = [
+      [80.499550, -167.276413], //Southwest
+      [-5.162102, -2.233040]  //Northeast
+    ];
+
+    this.map = L.map('mapid', {
+      'maxBounds': maxBounds
+    }).setView([-104.4480126008, 50.7736985613], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      minZoom: 3,
+      maxZoom: 6
     }).addTo(this.map);
 
-    const formData = new FormData();
-    formData.append('past_year', 2012);
-    formData.append('past_week', 1);
 
+    const formData = new FormData();
+    formData.append('past_year', $("#year").val());
+    formData.append('past_week', $("#week").val());
 
     this.store.post('/visualise_past_data', formData).subscribe((res) => {
 
@@ -77,14 +105,14 @@ export class VizComponent implements OnInit {
 
     // method that we will use to update the control based on feature properties passed
     info.update = function(props) {
-      this._div.innerHTML = '<h4>Fishing Vessel Density</h4>' + (props ?
-        '<b>' + props.density + '</b>'
-        : 'Hover over a polygon');
+      this._div.innerHTML = '<h6>Fishing Vessel Density Range</h6><div><canvas id="doughchartContainer" style="height: 170px; width: 210px;"></canvas></div>';
     };
 
     info.addTo(map);
 
-    L.choropleth(this.geojsonData, {
+    var geojsonLayer;
+
+    geojsonLayer = L.choropleth(this.geojsonData, {
       valueProperty: 'density', // which property in the features to use
       scale: ['white', 'red'], // chroma.js scale - include as many as you like
       steps: 5, // number of breaks or steps in range
@@ -94,32 +122,65 @@ export class VizComponent implements OnInit {
         weight: 2,
         fillOpacity: 0.8
       },
-      onEachFeature: function(feature, layer) {
-        layer.on({
-          mouseover: ((e) => {
-            var layer = e.target;
-
-            layer.setStyle({
-              weight: 5,
-              color: '#666',
-              dashArray: '',
-              fillOpacity: 0.7
-            });
-
-            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-              layer.bringToFront();
-            }
-
-            info.update(layer.feature.properties);
-          }),
-          mouseout: ((e) => {
-            geojsonLayer.resetStyle(e.target);
-            info.update();
-          })
-        });
-        layer.bindPopup('Density ' + feature.properties.density);
-      }
+      onEachFeature: onEachFeature
     }).addTo(map)
+
+    info.update({ median: this.median });
+
+    this.drawDoughnut();
+
+    function onEachFeature(feature, layer) {
+      layer.on({
+        mouseover: ((e) => {
+          var layer = e.target;
+
+          layer.setStyle({
+            weight: 2,
+            color: '#aaa',
+            dashArray: '',
+            fillOpacity: 0.8
+          });
+
+          if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+          }
+        }),
+        mouseout: ((e) => {
+          geojsonLayer.resetStyle(e.target);
+        })
+      });
+      layer.bindPopup('Density: ' + feature.properties.density);
+    }
   }
+
+  drawDoughnut() {
+    var ctx = $("#doughchartContainer");
+    let median = this.median;
+    let max = this.max;
+    let doughColor = '#ffc107';
+
+    if ((median / max) < 25) {
+      doughColor = "#28a745";
+    }
+    else if ((median / max) > 70) {
+      doughColor = "#dc3545";
+    }
+    var myPieChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['Median', 'Max'],
+        datasets: [{
+          label: '# of Votes',
+          data: [this.median, this.max],
+          backgroundColor: [
+            doughColor
+          ],
+          //cutoutPercentage : ,
+          borderWidth: 5
+        }]
+      }
+    });
+  }
+
 
 }
